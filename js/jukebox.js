cf260b910efe3c4212946b56d07346864f72a85c
Cam Crews <camcrews@gmail.com>
2017-09-26 15:50:14 -0400

    C1 Jukebox Constructor

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,27 +1,36 @@
-const songs = [
-  "corruption.mp3",
-  "hitman.mp3",
-  "killers.mp3"
-];
-let currentSong = 0
-  , elAudio;
+
+function Jukebox(){
+  this.songs = [
+    "corruption.mp3",
+    "hitman.mp3",
+    "killers.mp3"
+  ];
+  this.currentSong = 0;
+}
+
+
+const myJukebox = new Jukebox();
 
 document.addEventListener("DOMContentLoaded",function(){
   elAudio = document.querySelector("audio");
   elAudio.play();
 
-  document.querySelector("#jukebox .controls .play").addEventListener("click",function(){
-    elAudio.play();
-  });
-  document.querySelector("#jukebox .controls .pause").addEventListener("click",function(){
-    elAudio.pause();
-  });
-  document.querySelector("#jukebox .controls .back").addEventListener("click",function(){
-    currentSong -= 1;
-    if( currentSong < 0) {
-      currentSong = songs.length - 1;
+  document.querySelector("#jukebox .controls").addEventListener("click",function(event){
+    if( event.target.classList.contains("play") ){
+      elAudio.play();
+    } else if( event.target.classList.contains("pause")) {
+      elAudio.pause();
+    } else if( event.target.classList.contains("back")) {
+      currentSong -= 1;
+      if( currentSong < 0) {
+        currentSong = songs.length - 1;
+      }
+      elAudio.src = `media/${songs[currentSong]}`;
+      elAudio.play();
+    } else if( event.target.classList.contains("next")) {
+      currentSong = (currentSong + 1) % songs.length;
+      elAudio.src = `media/${songs[currentSong]}`;
+      elAudio.play();
     }
-    elAudio.src = `media/${songs[currentSong]}`;
-    elAudio.play();
   });
 });
